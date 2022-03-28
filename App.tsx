6341a114315b6e1e058d97e29f2e58d79a902575
Guillaume TECHER <guillaumeval.techer@gmail.com>
2022-03-27 21:52:03 -0400

    feat: home screen re-design

--- App.tsx
+++ App.tsx
@@ -6,17 +6,38 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 import "react-native-get-random-values";
 import { StorageContextProvider } from "./components/contexts/StorageContext";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, extendTheme } from "native-base";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const theme = extendTheme({
+    colors: {
+      // Add new color
+
+      light: {
+        400: "#FFFDF9",
+      },
+      purple: {
+        400: "#231139",
+      },
+
+      accent: {
+        300: "#1BDE7A",
+        400: "#1BDE7A",
+        500: "#1BDE7A",
+        600: "#1BDE7A",
+        700: "#1BDE7A",
+      },
+    },
+  });
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <NativeBaseProvider>
+        <NativeBaseProvider theme={theme}>
           <StorageContextProvider>
             <Navigation colorScheme={colorScheme} />
             <StatusBar />
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -7,6 +7,9 @@ import {
   ScrollView,
   Flex,
   Text,
+  HStack,
+  Pressable,
+  Spacer,
 } from "native-base";
 import React, { useEffect, useState, useContext } from "react";
 import { Button } from "native-base";
@@ -16,8 +19,8 @@ import { secsToTime } from "../lib/time";
 import { useIsFocused } from "@react-navigation/native";
 import { StorageContext } from "../components/contexts/StorageContext";
 import { Step } from "../db/stepModel";
-import AppLoading from 'expo-app-loading';
-
+import AppLoading from "expo-app-loading";
+import { chunk } from "lodash";
 export default function Home({ navigation }: RootTabScreenProps<"Home">) {
   const { sequences, refreshSequences, ready } = useContext(StorageContext);
   const isFocused = useIsFocused(); //forces re-fetch when comming back to this page
@@ -28,70 +31,84 @@ export default function Home({ navigation }: RootTabScreenProps<"Home">) {
 
   function getTotalTime(steps: [Step]) {
     // seems a bit painfull to do in sql with type orm as it would lose the object mapping i guess
-    // update: actually it would not so this is a 
+    // update: actually it would not so this is a
     // TO-DO
     return steps.reduce((sum, step) => sum + step.time, 0);
   }
-if (!ready){
-  console.log('eh')
-  return <AppLoading/>
-}
+  if (!ready) {
+    return <AppLoading />;
+  }
   return (
-    <Center style={{ flex: 1 }}>
+    <View bg="light.400" style={{ flex: 1 }}>
       <View>
-        <Heading mt="5" mb="5">
-          Your sequences
+        <Heading color='purple.400'mx="7" my="5">
+          KRONOMETERS
         </Heading>
       </View>
       <ScrollView style={{ flex: 0.9 }}>
-        <VStack space={4} alignItems="center">
-          {sequences.map((sequence, index) => (
-            <Button
-              w="64"
-              h="10"
-              bg="emerald.500"
-              onPress={() =>
-                navigation.navigate("Run", {
-                  sequenceName: sequence.name,
-                  sequenceId: sequence.id,
-                  sequenceIndex: index,
-                })
-              }
-              key={sequence.name.replace(" ", "-")}
-              rounded="md"
-              shadow={3}
-            >
-              <Flex
-                w="230"
-                alignContent="center"
-                justifyContent="space-between"
-                direction="row"
-              >
-                <Text bold color="white">
-                  {sequence.name}
-                </Text>
-                <Text color="white">
-                  {" "}
-                  {secsToTime(getTotalTime(sequence.steps))}
-                </Text>
-              </Flex>
-            </Button>
+        <VStack space={6} alignItems="center">
+          {chunk(sequences, 2).map((sequencesChunk, index) => (
+            <HStack space={6} justifyContent="center">
+              {sequencesChunk.map((sequence, chunkIndex) => (
+                <Pressable
+                  w="150px"
+                  h="100px"
+                  bg="purple.400"
+                  onPress={() =>
+                    navigation.navigate("Run", {
+                      sequenceName: sequence.name,
+                      sequenceId: sequence.id,
+                      sequenceIndex: index+chunkIndex,
+                    })
+                  }
+                  key={sequence.name.replace(" ", "-")}
+                  rounded="xl"
+                  shadow={4}
+                >
+                  <Flex
+                    //  bg="green.400"
+                    justify="space-between"
+                    direction="column"
+                    mx="3"
+                    my="3"
+                    h="80px"
+                  >
+                    <Text bold color="light">
+                      {sequence.name}
+                    </Text>
+                    <Flex direction="row">
+                      <Spacer />
+                      <Text bold color="accent.400">
+                        {sequence.steps.length} steps
+                      </Text>
+                      <Text color="light">
+                        {" "}
+                        {secsToTime(getTotalTime(sequence.steps))}
+                      </Text>
+                    </Flex>
+                  </Flex>
+                </Pressable>
+              ))}
+            </HStack>
           ))}
         </VStack>
       </ScrollView>
-      <Button
-        w="64"
-        h="15"
-        style={{ flex: 0.1 }}
-        marginBottom="6"
-        marginTop="3"
-        bg="primary.500"
-        onPress={() => navigation.navigate("Create")}
-        rounded="md"
-        shadow={3}
-      >
-        Add
-      </Button>
-    </Center>
+      <Flex style={{ flex: 0.25 }} direction="row">
+        <Spacer />
+        <Button
+          h="80px"
+          w="80px"
+          marginBottom="6"
+          marginRight="6"
+          colorScheme="accent"
+          onPress={() => navigation.navigate("Create")}
+          rounded="lg"
+          variant="outline"
+          _text={{ fontSize:"3xl"}}
+        >
+        +
+        </Button>
+      </Flex>
+    </View>
   );
 }
